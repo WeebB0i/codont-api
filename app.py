2fd2e1082312400dc26946c03c08b017221e9838
germandc <germancelis16@gmail.com>
2024-11-17 19:44:00 -0500

    Agregar middleware CORS para permitir orígenes específicos

--- app.py
+++ app.py
@@ -8,12 +8,27 @@ from routes.comparativas import comparativas
 from fastapi import FastAPI
 # from pyngrok import ngrok
 from routes.vehiculos import vehiculos
+from fastapi.middleware.cors import CORSMiddleware
 
 # Inicia el túnel Ngrok
 # public_url = ngrok.connect(8000)
 # print(f"Tu API está disponible en: {public_url}")
 
 app = FastAPI()
+
+# Configura los orígenes permitidos
+origins = [
+    "http://localhost:4321",
+    # Agrega otros orígenes si es necesario
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
